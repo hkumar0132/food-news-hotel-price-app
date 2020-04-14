d158a19d921adf101883f07a80bd5278e5277338
hkumar0132 <hkumar0132@gmail.com>
2020-04-14 14:44:55 +0530

    generated deployables

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -12,7 +12,7 @@ import { DBService } from '../mango-db-service.service';
   styleUrls: ['./home.component.css']
 })
 
-export class HomeComponent {
+export class HomeComponent implements OnInit {
   
   address;
   city : string = '';
@@ -20,13 +20,25 @@ export class HomeComponent {
   checkout : Date;
   roomtype;
   claims;
+  username = 'ABC';
+  password = 'ahe%ejape';
 
   constructor(private oauthService : OAuthService, 
-     private apiService : ApiService, private router : Router, private cat : CatService,
+     private apiService : ApiService, private router : Router, private jwt : JwtService,
      private dbService : DBService) { 
 
   }
-
+ 
+  ngOnInit() {
+    this.jwt
+    .login(this.username, this.password)
+    .subscribe(
+    (data) => {
+      // console.log(data);
+    }
+  );
+  this.dbService.initDB();
+}
   navigate() {
     this.apiService.getLocationSuggestion(this.address)
     .subscribe(data => {
